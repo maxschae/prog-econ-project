20579267981143fbed03ebad326c1fafe43cc294
Caroline Krayer <caroline.krayer@t-online.de>
2020-02-07 17:07:30 +0100

    Add local polynomial regression function with first test.

---
+++ src/functions/local_polynomial.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def y_hat_local_polynomial(x, y, x0, degree=1, bandwidth=1):
+    """ Perform local polynomial regression with the triangle kernel to
+        predict the value of the dependent variable at some point x0.
+
+    Args:
+        x (np.array): Array containing regressor values used for regression.
+        y (np.array): Array containing dependent variable used for regression.
+        x0 (float): Value at which local polynomial regression is calculated.
+        degree (float): Degree of polynomial used in local regression.
+        bandwidth (float): Range of data the kernel uses to assign weights.
+    Returns:
+        y0_hat (float): Predicted value of the dependent variable at x0.
+    """
+
+    # Assign weights to observations.
+    data_points = np.abs(x - x0) / bandwidth
+    weights = np.zeros_like(data_points)
+    index = np.where(np.abs(data_points) <= 1)
+    weights[index] = 1 - np.abs(data_points[index])
+
+    # Filter out datapoints with zero weights.
+    index2 = np.where(np.abs(weights) > 1e-10)[0]
+    weights = weights[index2]
+    x = x[index2]
+    y = y[index2]
+
+    # Compute predicted value y0.
+    sqrt_weights = np.sqrt(weights)
+    X = x[:, None] ** np.arange(degree + 1)
+    X0 = x0 ** np.arange(degree + 1)
+
+    X_weighted = X * sqrt_weights[:, None]
+    y_weighted = y * sqrt_weights
+
+    reg_results = np.linalg.lstsq(X_weighted, y_weighted, rcond=None)
+    beta_hat = reg_results[0]
+    y0_hat = X0.dot(beta_hat)
+
+    return y0_hat
---
+++ src/functions/test_local_polynomial.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+from local_polynomial import y_hat_local_polynomial
+
+
+@pytest.fixture
+def setup_local_polynomial():
+    out = {}
+    out["x"] = np.array([2, 1, 1.75, 1.5, 3, 3.5, 4], dtype=np.float64)
+    out["y"] = np.array([3, 4, 3, 3.5, 6, 6, 7], dtype=np.float64)
+    out["x0"] = 1.4
+    out["degree"] = 1
+    out["bandwidth"] = 1
+
+    return out
+
+
+@pytest.fixture
+def expected_local_polynomial():
+    out = {}
+    out["y0_hat"] = 3.552056
+
+    return out
+
+
+def test_local_polynomial_y0_hat(setup_local_polynomial, expected_local_polynomial):
+    calc_y0_hat = y_hat_local_polynomial(
+        x=setup_local_polynomial["x"],
+        y=setup_local_polynomial["y"],
+        x0=setup_local_polynomial["x0"],
+        degree=setup_local_polynomial["degree"],
+        bandwidth=setup_local_polynomial["bandwidth"],
+    )
+    assert np.isclose(calc_y0_hat, expected_local_polynomial["y0_hat"])
